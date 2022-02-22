b75654787f26bcaaec1f6c809a6e2a838ee9dbb9
Klairgo <67203878+Klairgo@users.noreply.github.com>
2022-02-22 13:39:15 +0200

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test{
-    public static void main(String[] args) {
-         Main m1 = new Main(2);
-         m1.printValues();
-         Tester t1 = new Tester(3);
-         t1.printValues();
-    } 
-}
